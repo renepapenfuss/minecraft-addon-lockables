d63822082ed3e92b2219ea89137e9690d73c1c45
René Papenfuß <rene.papenfuss@live.de>
2025-06-06 17:23:14 +0100

    fixed a bug that prevented the locked containers to saved and loaded correctly

--- scripts/services/locked-containers.service.ts
+++ scripts/services/locked-containers.service.ts
@@ -12,11 +12,12 @@ export function loadLockedContainers() {
     const jsonData = world.getDynamicProperty(config.dynamicPropertyName);
 
     if (typeof jsonData === "string" && jsonData.length > 0) {
-      const savedChests = JSON.parse(jsonData);
+      const storedLockedContainers = JSON.parse(jsonData);
 
-      Object.keys(savedChests).forEach((key) => {
-        lockedContainers.set(key, savedChests[key]);
-      });
+      for (const [locationStr, containerName] of storedLockedContainers) {
+        const location = JSON.parse(locationStr);
+        lockedContainers.set(JSON.stringify(location), containerName);
+      }
     } else {
       console.warn("Keine gespeicherten Kisten gefunden.");
     }
@@ -28,7 +29,7 @@ export function loadLockedContainers() {
 
 export function saveLockedContainers() {
   try {
-    const jsonData = JSON.stringify(lockedContainers);
+    const jsonData = JSON.stringify(Array.from(lockedContainers.entries()));
     world.setDynamicProperty(config.dynamicPropertyName, jsonData);
   } catch (error) {
     console.warn("Fehler beim Speichern der Kisten: " + error);
